ae6bd5f89d17036dee78b598fed6c3f4e00a5fc3
b1est <bondarenko2001@gmail.com>
2022-10-20 12:31:12 +0100

    P(C) and P(M, C) for for variants 6 and 13

---
+++ lab1/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import os
+
+
+
+vars = (6, 13)
+
+
+def p_C(v, p, t):                           # P(C)
+    res = np.zeros(20)
+    for i in range(20):
+        for j in range(20):
+            c = t[i][j]
+            res[c] += p[0][j]*p[1][i]
+    np.savetxt(f"lab1/p_C_var{v}.csv", res, delimiter=";", fmt='%.6g')
+  
+    return res
+
+def p_MC(v, p, t):                      # Р(М, С)
+    res = np.zeros((20, 20))
+    _t = t.T
+    for i in range(20):
+        m_i = _t[i]
+        for ii in range(20):
+            res[i][m_i[ii]] += p[1][ii]*p[0][i]
+            
+    np.savetxt(f"lab1/p_MC_var{v}.csv", res, delimiter=";", fmt='%.6g')       
+    return res
+
+def main():
+    dir_name = "lab1/"
+    test = os.listdir(dir_name)
+
+    for item in test:
+        if item.endswith(".csv"):
+            os.remove(os.path.join(dir_name, item))
+    
+    for v in vars:
+        if v == 6:
+            prob = np.genfromtxt("lab1\csv\prob_06.csv", delimiter=',')
+            table =  np.genfromtxt("lab1\csv\/table_06.csv", delimiter=',', dtype=int)
+        elif v == 13:
+            prob = np.genfromtxt("lab1\csv\prob_13.csv", delimiter=',')
+            table =  np.genfromtxt("lab1\csv\/table_13.csv", delimiter=',', dtype=int)
+        pc = p_C(v, prob, table)
+        pmc = p_MC(v, prob, table)
+        
+        print(pc)
+        print()
+        print(pmc)
+main()
